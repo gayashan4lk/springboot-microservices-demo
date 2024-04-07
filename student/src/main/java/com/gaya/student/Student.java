ffe268c1bdbad91411b3817c6d9f0183ec13137b
gayashan4lk <gayashan4lk@gmail.com>
2024-04-08 00:07:55 +0530

    create student entity

---
+++ student/src/main/java/com/gaya/student/Student.java
@@ -0,0 +1,25 @@
+package com.gaya.student;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Student {
+
+    @Id
+    private Integer id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String schoolId;
+}
